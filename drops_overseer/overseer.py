26168b40bad37d90e50f2a6604952d0ffc2cf63d
Artem Leshchenko <art.leshch@gmail.com>
2024-07-08 12:00:30 +0300

    moving the api client to an async lib

--- drops_overseer/overseer.py
+++ drops_overseer/overseer.py
@@ -4,7 +4,7 @@ from types import SimpleNamespace
 
 import hashlib
 
-import requests
+import aiohttp
 
 from .utils import constants
 
@@ -28,24 +28,19 @@ class Overseer:
     """TODO: docstring"""
 
     _QUERIES = SimpleNamespace(
-        ViewDropCampaigns=SimpleNamespace(
-            operation="ViewDropCampaigns",
-            hash=""
-        ),
-        ViewCampaignDetails=SimpleNamespace(
-            operation="ViewCampaignDetails",
-            hash=""
-        ),
+        ViewDropCampaigns=SimpleNamespace(operation="ViewDropCampaigns", hash=""),
+        ViewCampaignDetails=SimpleNamespace(operation="ViewCampaignDetails", hash=""),
     )
 
     def __init__(self):
         self._client_id = constants.CLIENT_ID
         self._auth_token = constants.AUTH_TOKEN
 
-    def _make_authorized_request(self, operation: str, query_hash: str, variables: dict=None):
+    async def _make_authorized_request(
+        self, operation: str, query_hash: str, variables: dict = None
+    ):
         headers = {
             "Content-Type": "text/plain;charset=UTF-8",
-            # "Client-ID": CLIENT_ID
             "Authorization": f"OAuth {self._auth_token}",
         }
 
@@ -60,22 +55,31 @@ class Overseer:
             },
         }
 
-        r = requests.post(constants.GQL_API_URL, timeout=30, headers=headers, json=data)
-        print(r.text)
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                url=constants.GQL_API_URL,
+                headers=headers,
+                json=data,
+            ) as response:
+                print(await response.text())
 
-    def get_drop_campaigns(self) -> list:
+    @classmethod
+    async def get_drop_campaigns(cls) -> list:
         """TODO: docstring"""
-        
+
+        instance = cls()
+
         hash_object = hashlib.sha256(QUERY_STRING.encode("utf-8"))
         query_hash = hash_object.hexdigest()
 
-        self._make_authorized_request(
-            self._QUERIES.ViewDropCampaigns.operation, query_hash
+        await instance._make_authorized_request(
+            instance._QUERIES.ViewDropCampaigns.operation, query_hash
         )
 
         return []
 
-    def get_campaign_details(self, id: int) -> list:
+    @classmethod
+    async def get_campaign_details(cls, id: int) -> list:
         """TODO: docstring"""
 
         # self._make_authorized_request(
